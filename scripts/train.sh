aa8bc6ecbfba5afd1d9b714e5fff5d6cdadc4f0c
Ali Sedaghi <ali_sedaghi@ymail.com>
2026-02-14 13:59:40 +0330

    Add runner scripts for train and inference

---
+++ scripts/train.sh
@@ -0,0 +1,11 @@
+python ./scripts/train.py ./models/brats/segformerB3.py\
+  --exp-name=Test\
+  --epochs=2\
+  --batch-size=2\
+  --ngpus=0\
+  --cpu\
+  --workers=2\
+  --datapath=./data/datasets/BraTS\
+  --channel=mix\
+  --label=wt\
+  --pretrained
